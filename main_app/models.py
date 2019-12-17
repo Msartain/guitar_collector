f985580432c185d6a8dac28f6ca237a982bea357
Matthew Sartain <matthewsartain@me.com>
2019-12-16 21:13:15 -0600

    finished lab 3 - added update/delete guitar view functions

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -10,3 +11,6 @@ class Guitar(models.Model):
     
     def __str__(self):
         return f'{self.brand} {self.model} ({self.id})'
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'guitar_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Guitar
 
 # Create your views here.
@@ -18,3 +19,14 @@ def guitars_detail(request, guitar_id):
     guitar = Guitar.objects.get(id=guitar_id)
     return render(request, 'guitars/detail.html', { 'guitar' : guitar })
 
+class GuitarCreate(CreateView):
+    model = Guitar
+    fields = '__all__'
+    
+class GuitarUpdate(UpdateView):
+    model = Guitar
+    fields = '__all__'
+    
+class GuitarDelete(DeleteView):
+    model = Guitar
+    sucess_url = '/guitars/'
